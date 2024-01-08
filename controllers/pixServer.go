10a79ebac1531b2cb871d70b7edc75472268783a
edwardOWO <a0981202524@gmial.com>
2024-01-07 23:09:35 -0500

    fix

--- controllers/pixServer.go
+++ controllers/pixServer.go
@@ -410,6 +410,7 @@ func (u *Server) LoginHandler(c *gin.Context) {
 		token := u.utils.GenerateJWTToken(username)
 		c.SetCookie("jwt", token, 360000, "/", "localhost", false, true)
 		c.SetCookie("jwt", token, 360000, "/", u.Memberlist.LocalNode().Addr.String(), false, true)
+		c.SetCookie("jwt", token, 360000, "/", "60.199.173.12", false, true)
 		c.Redirect(http.StatusSeeOther, "/index")
 	} else {
 		c.Redirect(http.StatusSeeOther, "/?error=InvalidCredentials")
